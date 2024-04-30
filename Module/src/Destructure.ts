9884477c2fe6ce401ef3046dc2b934b1a59a3be6
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-05-01 02:28:09 +0800

    explore destructuring object and array

---
+++ Module/src/Destructure.ts
@@ -0,0 +1,32 @@
+// destructing details
+
+{
+
+    //Object destructring
+    const user = {
+        id: 2047,
+        name: {
+            firstName: 'Rasel',
+            lastName: 'Chowdhury'
+        },
+        contact: '01834945',
+        address: 'Bangladesh'
+    }
+
+    const {
+        contact, 
+        name: {firstName : fName} //set fName of firstName.it called type alias
+    }
+     = user;
+
+     //Array Destructuring
+     const friends = ['jabed','nazrul','rajesh','obishek', 'durjoy'];
+
+     //now i want to best friend rajesh catch
+
+     const [, , bestfrined] = friends;
+     console.log(bestfrined)//output will be rajesh
+
+     //if i want others elements recive after rajesh then use ...rest
+     const [, , bestfri, ...rest] = friends
+}
