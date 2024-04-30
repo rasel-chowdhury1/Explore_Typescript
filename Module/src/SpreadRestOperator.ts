d488f9a2193a13f99b0d2f491938016325d1a8ef
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-05-01 02:10:57 +0800

    explore spread and rest operator

--- Module/src/SpreadRestOperator.ts
+++ Module/src/SpreadRestOperator.ts
@@ -18,5 +18,39 @@
         'shovon'
     ]
 
+    bros1.push(...bros2)
+
+    //object
+    const mentors1 = {
+        typescript: 'Mezba',
+        redux: 'Mir',
+        dbms: 'Mizan'
+    }
+
+    const mentors2 = {
+        prisma: 'Firoz',
+        nextjs: 'tonmoy',
+        cloud: 'Nahid'
+    }
+
+    const mentorList = {
+        ...mentors1,
+        ...mentors2
+    }
+
+
+    //learn rest operator
+    
+    // this function recive just fixed parameter but we want any number parameters recive in this function then use ...rest 
+    //when ...rest operator then this function recive list of array []
+    const greatFriends = (...friends: string[]) => {
+       friends.forEach((friend) => console.log(`hi ${friend}`)) 
+    }
+
+    
+    greatFriends('rajesh','shovon', 'kowshick');
+
+
+
 
 }
