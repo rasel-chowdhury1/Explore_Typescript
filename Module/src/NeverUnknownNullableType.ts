fd3223219cc5dee2488b0741966ee42abc8505bc
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-05-01 19:51:46 +0800

    explore nullable | unknown | never type

---
+++ Module/src/NeverUnknownNullableType.ts
@@ -0,0 +1,37 @@
+{
+
+// nullable type
+
+const searchName = (value: string | null) => {
+    if(value){
+        console.log('Searching');
+    }
+    else{
+        console.log('There is nothing to search')
+    }
+}
+
+searchName(null)
+
+//unknown Type
+const getSpeedMeterInPerSecond = (value: unknown) => {
+    if (typeof value === 'number'){
+        const convertedSpeed = value*1000/3600;
+        console.log(`The speed is ${convertedSpeed} ms^-1`)
+    }
+    else if (typeof value === 'string') {
+        const valueInNumber = parseFloat(value.split(' ')[0]);
+        const convertedSpeed = (valueInNumber*1000/3600).toFixed(2);
+        console.log(`the speed is ${convertedSpeed} of ${value}`)
+    }
+}
+
+getSpeedMeterInPerSecond('1000 kmh^-1');
+
+//never type
+const throwError = (msg: string): never => {
+    throw new Error(msg)
+}
+
+throwError("mushkill se error hogaya")
+}
