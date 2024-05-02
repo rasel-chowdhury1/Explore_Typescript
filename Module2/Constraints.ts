ce03d79cb7ebe2b689063cec699ab2c04a7f07d0
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-05-02 22:05:46 +0600

    constraint of typescript

---
+++ Module2/Constraints.ts
@@ -0,0 +1,26 @@
+{
+  // constraints is something do force.
+  type Student = {
+    id: number,
+    name: string,
+    email: string
+  }
+  // this is called constraint(must pass 3 property(id,name,email) of object)
+  const addCourseToStudent = <T extends Student>(student: T) => {
+    const course = "Next level Web development";
+
+    return {
+        ...student,
+        course
+    }
+
+  }
+
+  const student1 = addCourseToStudent({id: 101, name: 'sayed',email:'sayed@gmail.com', dev: 'next level developer' })
+
+  const student2 = addCourseToStudent({id:102, name:'parvez', email: 'parvez@gmail.com', dev: 'front end developer'})
+  
+  //when one or more property pass missing show error
+  const student3 = addCourseToStudent({id:103, name:"xyz", email:'xyz@gmail.com'})
+
+}
